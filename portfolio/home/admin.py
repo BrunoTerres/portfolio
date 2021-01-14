2c2a74b05ef037058ccd66c30f77887d22ce49d4
BrunoTerres <brunoeterres@gmail.com>
2021-01-13 21:10:25 -0300

    Home page with dynamic content created

--- portfolio/home/admin.py
+++ portfolio/home/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import DevIntro, ProgrammingLanguages, Technologies
+from .models import DevIntro, ProgrammingLanguages, Technologies, Projects
 
 admin.site.register(DevIntro)
 admin.site.register(ProgrammingLanguages)
 admin.site.register(Technologies)
+admin.site.register(Projects)
--- portfolio/home/views.py
+++ portfolio/home/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+#from django.views import generic
 
-# Create your views here.
+from home.models import DevIntro, ProgrammingLanguages, Projects, Technologies
+
+def index(request):
+    dev = DevIntro.objects.get(id=1)
+    projects = Projects.objects.all()
+    languages = ProgrammingLanguages.objects.all()
+    technologies = Technologies.objects.all()
+
+    context = {
+        'dev': dev,
+        'projects': projects,
+        'languages': languages,
+        'technologies': technologies,
+    }
+
+    return render(request, 'home/index.html', context)
+
+
+# class IndexView(generic.TemplateView):
+#     dev = DevIntro.objects.all()
+
+#     context = {
+#         'dev': dev,
+#     }
+    
+#     template_name = "home/index.html"
