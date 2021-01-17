8acae003fb5adefa2fe7530bdd252e7e2a446262
BrunoTerres <brunoeterres@gmail.com>
2021-01-17 16:48:37 -0300

    bug fix

--- portfolio/home/models.py
+++ portfolio/home/models.py
@@ -6,8 +6,7 @@ class Bio(models.Model):
     name = models.CharField(max_length=100)
     bio = models.TextField()
     avatar = models.ImageField()
-            
-    
+
     def __str__(self):
         return '{0}'.format(self.name) 
 
@@ -16,7 +15,6 @@ class Language(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     img =  models.ImageField()
-    bio_language = models.ForeignKey("Bio", on_delete=models.SET_NULL, null=True, blank=True)
 
     def get_absolute_url(self):
         return reverse('language_detail', args=[str(self.id)])
@@ -27,9 +25,8 @@ class Language(models.Model):
 
 class Tool(models.Model):
     name = models.CharField(max_length=100)
-    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True, blank=True)
+    language = models.ForeignKey(Language, on_delete=models.SET_NULL, null=True, blank=True)
     description = models.TextField()
-    bio_tool = models.ForeignKey("Bio", on_delete=models.SET_NULL, null=True, blank=True)
 
     def get_absolute_url(self):
         return reverse('tool_detail', args=[str(self.id)])
@@ -40,8 +37,8 @@ class Tool(models.Model):
 class Job(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    Tool = models.ManyToManyField('Tool', help_text="Tool used in the Job")
-    bio_job = models.ForeignKey("Bio", on_delete=models.SET_NULL, null=True, blank=True)
+    Tool = models.ManyToManyField(Tool, help_text="Tool used in the Job")
+
 
     def display_tool(self):
         return ', '.join(tool.name for genre in self.tool.all())
