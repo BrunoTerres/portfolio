442cffd0d95a17ec68072b9999cd39d07136ce4c
BrunoTerres <brunoeterres@gmail.com>
2021-01-18 18:41:45 -0300

    PostgreSQL megration

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -74,13 +74,32 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'portfolio',
+
+        'USER': 'bruno',
+
+        'PASSWORD': 'm3n#',
+
+        'HOST': '127.0.0.1',
+
+        'PORT': '5432',
+
+    }
+
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
