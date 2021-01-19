d951138420756c29de4921c4584f8c3c11b5af6c
BrunoTerres <brunoeterres@gmail.com>
2021-01-19 15:19:57 -0300

    Links and previous page button done

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -82,23 +82,14 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # }
 
 DATABASES = {
-
-    'default': {
-
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-        'NAME': 'portfolio',
-
-        'USER': 'bruno',
-
-        'PASSWORD': 'm3n#',
-
-        'HOST': '127.0.0.1',
-
-        'PORT': '5432',
-
-    }
-
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+         'NAME': 'portfolio',
+         'USER': 'bruno',
+         'PASSWORD': 'm3n#',
+         'HOST': '127.0.0.1',
+         'PORT': '5432',
+     }
 }
 
 # Password validation
